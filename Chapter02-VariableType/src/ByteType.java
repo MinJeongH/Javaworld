09a8466865f000e3d7ee3bfcb822c044706e6fd6
MinJeongH <90035652+MinJeongH@users.noreply.github.com>
2021-10-07 09:25:45 +0900

    share test

---
+++ Chapter02-VariableType/src/ByteType.java
@@ -0,0 +1,23 @@
+/*
+  바이트 타입
+  1바이트 단위 : -128 ~ 1227 값을 저장할 수 있는 공간
+ */
+public class ByteType {
+
+	public static void main(String[] args) {
+		byte b1 =0;
+		byte minus = -128;
+		byte plus = 127;
+		byte abc = 123;
+		byte b2;
+		
+		b2 = 25;
+		
+		System.out.println(b1);
+		System.out.println(minus);
+		System.out.println(plus);
+		System.out.println(abc);
+		System.out.println(b2);
+	}
+
+}
